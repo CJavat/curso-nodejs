13442efc8c1021e1bcd225a35162fbd5a4c65862
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2023-02-16 06:10:35 -0600

    Seccion 6 - TERMINADO

--- bienesraices_mvc/index.js
+++ bienesraices_mvc/index.js
@@ -3,8 +3,15 @@ import usuarioRoutes from "./routes/usuario.routes.js";
 // Crear la app.
 const app = express();
 
+// Habilitar pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Carpeta Publica.
+app.use(express.static("public"));
+
 // Routing.
-app.use("/", usuarioRoutes);
+app.use("/auth", usuarioRoutes);
 
 // Definir un puerto y arrancar el proyecto.
 const PORT = 3000;
--- bienesraices_mvc/routes/usuario.routes.js
+++ bienesraices_mvc/routes/usuario.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
+import {
+  formularioLogin,
+  formularioRegistro,
+} from "../controllers/usuario.controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ msg: "Hola mundo en express." });
-});
+router.get("/login", formularioLogin);
+
+router.get("/registro", formularioRegistro);
 
 export default router;
