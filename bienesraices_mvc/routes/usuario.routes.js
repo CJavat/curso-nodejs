a9d81c27babe30d0302c6806e742a125cb023f37
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2023-02-16 06:38:08 -0600

    Seccion 6 - TERMINADO

--- bienesraices_mvc/routes/usuario.routes.js
+++ bienesraices_mvc/routes/usuario.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   formularioLogin,
   formularioRegistro,
+  formularioOlvidePassword,
 } from "../controllers/usuario.controller.js";
 
 const router = express.Router();
@@ -9,5 +10,7 @@ const router = express.Router();
 router.get("/login", formularioLogin);
 
 router.get("/registro", formularioRegistro);
+
+router.get("/olvide-password", formularioOlvidePassword);
 
 export default router;
