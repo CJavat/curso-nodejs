6ad9d8d2151b0936f899137bdc635b210cc9536f
JavatoX <cdpm98@hotmail.com>
2023-02-22 17:28:50 -0600

    Seccion 29 - EN CURSO

--- DevJobs/index.js
+++ DevJobs/index.js
@@ -18,6 +18,7 @@ app.engine(
   "handlebars",
   exphbs.engine({
     defaultLayout: "layout",
+    helpers: require("./helpers/handlebars"),
   })
 );
 app.set("view engine", "handlebars");
--- DevJobs/routes/index.routes.js
+++ DevJobs/routes/index.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home.controller");
+const {
+  formularioNuevaVacante,
+} = require("../controllers/vacantes.controller");
 
 module.exports = () => {
   router.get("/", homeController.mostrarTrabajos);
+
+  // Crear Vacantes.
+  router.get("/vacantes/nueva", formularioNuevaVacante);
 
   return router;
 };
