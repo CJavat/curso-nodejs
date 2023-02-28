4514f5fdf855ee408c595c8151064f4cefa5a04c
DanielPlascencia <javat.cursos@gmail.com>
2023-02-27 23:22:09 -0600

    SECCION 70 - EN CURSO

--- restapis/index.js
+++ restapis/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const routes = require("./routes/index.routes");
+const mongoose = require("mongoose");
+
+// Conectar Mongo.
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://root:root@fsociety.1mrzupj.mongodb.net/restapis",
+  {
+    useNewUrlParser: true,
+  }
+);
 // Crear el servidor
 const app = express();
 
